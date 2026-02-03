ce671e8d79d8235ed6608ecaf716f6c79be89370
Prethewram <prithewramprithew1@gmail.com>
2026-02-03 22:47:07 +0530

    firstff

--- device/views.py
+++ device/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics ,status 
 from .models import *
 from .serilaizers import *
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 class listview(generics.ListCreateAPIView):
     queryset = devicedetails.objects.all()
     serializer_class = listserializer
+    pagination_class = PageNumberPagination
 
 class addname(generics.ListCreateAPIView):
     queryset = devicedetails.objects.all()
     serializer_class = listserializer
+    pagination_class = PageNumberPagination
